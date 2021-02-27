e70ab908139f362f43d83a8d5c1d1aa952b05ebc
ekayesorko <ekayesorko@gmail.com>
2021-02-28 01:34:16 +0600

    modified for thesis input

--- suraiya.py
+++ suraiya.py
@@ -1,28 +1,24 @@
 import pickle
 from bs4 import BeautifulSoup
 
+def get_data():
+    with open('facebookappreview.html') as html_file:
+        soup = BeautifulSoup(html_file, 'html.parser')
+    
+    mainList = soup.find('div', jsname = 'fk8dgd')
+    reviewList = mainList.findAll('span', jsname = 'fbQN7e')
+    starList = mainList.findAll('div', class_ = 'pf5lIe')
 
-with open('facebookappreview.html') as html_file:
-    soup = BeautifulSoup(html_file, 'html.parser')
- 
-mainList = soup.find('div', jsname = 'fk8dgd')
-reviewList = mainList.findAll('span', jsname = 'fbQN7e')
-starList = mainList.findAll('div', class_ = 'pf5lIe')
-print(len(reviewList))
-arr = [0,0,0,0,0]
-storageArray = []
- 
-for review, star in zip(reviewList, starList):
-    negCount = len(star.findAll('div', class_ = 'L0jl5e bUWb7c'))
-    posCount = 5 - negCount
-    reviewtype = posCount > 3
-    arr[posCount - 1] += 1
-    reviewString = review.text
-    totalReview = (reviewString, reviewtype)
-    storageArray.append(totalReview)
-    #print(reviewString, 5 - negCount, sep = "\n")
-print(storageArray)
-pickle.dump(storageArray, open("MessengerReview.p",'wb'))
-anotherStorageArray = pickle.load(open("MessengerReview.p","rb"))
-print(len(anotherStorageArray))
-print(arr)
+    text = []
+    sentiment = []
+    for review, star in zip(reviewList, starList):
+        negCount = len(star.findAll('div', class_ = 'L0jl5e bUWb7c'))
+        s = True if negCount < 3 else False
+        sentiment.append(s)
+        text.append(review)
+    
+    return (text, sentiment)
+
+if __name__ == "__main__":
+    text, sentiment = get_data()
+    print(len(sentiment))
